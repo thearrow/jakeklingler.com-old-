deddb5586ef33338a037b273338bc2e5950f82fa
Jake Klingler <thearrow3456@gmail.com>
2013-07-28 16:27:31 -0500

    simplify index

--- index.php
+++ index.php
@@ -6,121 +6,14 @@
 	<link type="text/css" rel="stylesheet" href="//cdn.jsdelivr.net/foundation/4.3.1/css/normalize.css" />
 	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/foundation/4.3.1/css/foundation.min.css">
 	<script type="text/javascript" src="//cdn.jsdelivr.net/foundation/4.3.1/js/vendor/custom.modernizr.js" async></script>
-
-	<title>Jake Klingler | Developer</title>
+	<title>Jake Klingler | Programmer</title>
 </head>
 <body>
 
-	<!-- Header and Nav -->
-	<nav class="top-bar">
-		<ul class="title-area">
-			<!-- Title Area -->
-			<li class="name">
-				<h1>
-					<a href="#">
-						Top Bar Title
-					</a>
-				</h1>
-			</li>
-			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-		</ul>
-
-		<section class="top-bar-section">
-			<!-- Left Nav Section -->
-			<ul class="left">
-				<li class="divider"></li>
-				<li class="has-dropdown">
-					<a class="active" href="#">Main Item 1</a>
-					<ul class="dropdown">
-						<li><label>Section Name</label></li>
-						<li><a href="#" class="">Dropdown Level 1</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li class="divider"></li>
-						<li><label>Section Name</label></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li class="divider"></li>
-						<li><a href="#">See all &rarr;</a></li>
-					</ul>
-				</li>
-				<li class="divider"></li>
-				<li><a href="#">Main Item 2</a></li>
-				<li class="divider"></li>
-				<li class="has-dropdown">
-					<a href="#">Main Item 3</a>
-					<ul class="dropdown">
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li class="divider"></li>
-						<li><a href="#">See all &rarr;</a></li>
-					</ul>
-				</li>
-
-			</ul>
-
-			<!-- Right Nav Section -->
-			<ul class="right">
-				<li class="divider"></li>
-				<li class="has-dropdown">
-					<a href="#">Main Item 4</a>
-					<ul class="dropdown">
-						<li><label>Section Name</label></li>
-						<li class="has-dropdown">
-							<a href="#" class="">Has Dropdown, Level 1</a>
-							<ul class="dropdown">
-								<li><a href="#">Dropdown Options</a></li>
-								<li><a href="#">Dropdown Options</a></li>
-								<li class="has-dropdown">
-									<a href="#">Has Dropdown, Level 2</a>
-									<ul class="dropdown test">
-										<li><a href="#">Subdropdown Option</a></li>
-										<li><a href="#">Subdropdown Option</a></li>
-										<li><a href="#">Subdropdown Option</a></li>
-									</ul>
-								</li>
-								<li><a href="#">Subdropdown Option</a></li>
-								<li><a href="#">Subdropdown Option</a></li>
-								<li><a href="#">Subdropdown Option</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li class="divider"></li>
-						<li><label>Section Name</label></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li class="divider"></li>
-						<li><a href="#">See all &rarr;</a></li>
-					</ul>
-				</li>
-				<li class="divider"></li>
-				<li><a href="#">Main Item 5</a></li>
-				<li class="divider"></li>
-				<li class="has-dropdown">
-					<a href="#">Main Item 6</a>
-					<ul class="dropdown">
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li><a href="#">Dropdown Option</a></li>
-						<li class="divider"></li>
-						<li><a href="#">See all &rarr;</a></li>
-					</ul>
-				</li>
-			</ul>
-		</section>
-	</nav>
-
-
-	<!-- End Header and Nav -->
-
-	<div class="row large-12 columns text-center">
+	<div class="row text-center">
 		<h1>Jake Klingler</h1>
+		<h3 class="subheader">New Site Coming Soon...</h3>
 	</div>
-
 
 	<!-- FOUNDATION -->
 	<script src="//cdn.jsdelivr.net/foundation/4.3.1/js/vendor/zepto.js"></script>
