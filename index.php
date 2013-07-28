1b5880384a8865bda54c02419247842e9dce0491
Jake Klingler <thearrow3456@gmail.com>
2013-07-28 17:27:43 -0500

    google web fonts

--- index.php
+++ index.php
@@ -4,9 +4,29 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width" />
 	<link type="text/css" rel="stylesheet" href="//cdn.jsdelivr.net/foundation/4.3.1/css/normalize.css" />
-	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/foundation/4.3.1/css/foundation.min.css">
-	<script type="text/javascript" src="//cdn.jsdelivr.net/foundation/4.3.1/js/vendor/custom.modernizr.js" async></script>
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/foundation/4.3.1/css/foundation.min.css" />
+	<script src="//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+	<script>
+	  WebFont.load({
+	    google: {
+	      families: ['Lato:300,400,700']
+	    }
+	  });
+	</script>
+	<style type="text/css">
+	h1 { 
+		font-family: 'Lato';
+		font-weight: 300;
+		font-size: 4em;
+	}
+	h3 {
+		font-family: 'Lato';
+		font-weight: 300;
+		font-size: 1.5em;
+	}
+	</style>
 	<title>Jake Klingler | Programmer</title>
+
 </head>
 <body>
 
@@ -16,6 +36,7 @@
 	</div>
 
 	<!-- FOUNDATION -->
+	<script src="//cdn.jsdelivr.net/foundation/4.3.1/js/vendor/custom.modernizr.js"></script>
 	<script src="//cdn.jsdelivr.net/foundation/4.3.1/js/vendor/zepto.js"></script>
 	<script src="//cdn.jsdelivr.net/foundation/4.3.1/js/foundation.min.js"></script>
 	<script>
