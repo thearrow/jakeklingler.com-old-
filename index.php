18dd5612fc0c047bd5a8ef8adeaa3dbbd1e5e77b
Jake Klingler <thearrow3456@gmail.com>
2013-08-02 22:02:15 -0500

    new social icons

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width" />
 	<link rel="stylesheet" type="text/css" href="scss/app.css"/>
-	<script src="js/app.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
 	<script>
 		WebFont.load({
@@ -24,20 +23,50 @@
 		</div>
 	</div>
 
-	<div class="row text-center icon-row">
-		<div class="image-container">
-			<a href="http://careers.stackoverflow.com/jakeklingler">
-				<img class="link-images" src="img/so.svg"/>
-			</a>
+	<div class="row text-center image-row">
+		<div class"small-6 small-centered columns">
+			<ul class="small-block-grid-3">
+				<li class="link-images">
+					<a href="http://github.com/thearrow">
+						<img src="img/webicon-github.svg"/>
+					</a>
+				</li>
+				<li class="link-images">
+					<a href="http://careers.stackoverflow.com/jakeklingler">
+						<img src="img/webicon-stackoverflow.svg"/>
+					</a>
+				</li>
+				<li class="link-images">
+					<ul class="small-block-grid-2">
+						<li class="link-images-small">
+							<a href="https://www.quora.com/Jake-Klingler">
+								<img src="img/webicon-quora.svg"/>
+							</a>
+						</li>
+						<li class="link-images-small">
+							<a href="https://www.facebook.com/jacob.klingler">
+								<img src="img/webicon-facebook.svg"/>
+							</a>
+						</li>
+						<li class="link-images-small">
+							<a href="http://www.goodreads.com/user/show/8126915-jake-klingler">
+								<img src="img/webicon-goodreads.svg"/>
+							</a>
+						</li>
+						<li class="link-images-small">
+							<a href="http://www.flickr.com/photos/9829286@N07/">
+								<img src="img/webicon-flickr.svg"/>
+							</a>
+						</li>
+					</ul>		
+				</li>
+			</ul>
 		</div>
-		<div class="image-container">
-			<a href="http://github.com/thearrow">
-				<img class="link-images" src="img/gh.svg"/>
-			</a>
-		</div>			
 	</div>
 
+
 	<!-- FOUNDATION -->
+	<script src="js/app.js"></script>
 	<script>
 		$(document).foundation();
 	</script>
