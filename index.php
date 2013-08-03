4b76238549eb5c034821d550802dd4beab26b938
Jake Klingler <thearrow3456@gmail.com>
2013-08-03 12:34:16 -0500

    flat link icons, different font, group-headers and hr's

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<script>
 		WebFont.load({
 			google: {
-				families: ['Lato:100,300']
+				families: ['Raleway:100,300']
 			}
 		});
 	</script>
@@ -19,43 +19,69 @@
 	<div class="top">
 		<div class="row text-center header-container">
 			<h1 class="header">JAKE KLINGLER</h1>
-			<h3 class="subheader small-header">New Site Coming Soon...</h3>
+			<h3 class="small-header">New Site Coming Soon...</h3>
 		</div>
 	</div>
 
-	<div class="row text-center image-row">
+	<div class="row text-center">
+		<div class="small-6 small-centered columns">
+			<h3 class="group-header">PROJECTS</h3>
+			<div class="hrule">
+				<span class="hrule-inner"></span>
+			</div>
+		</div>
+	</div>
+
+	<div class="row text-center">
+		<div class="small-6 small-centered columns">
+			<h3 class="group-header">RESUME</h3>
+			<div class="hrule">
+				<span class="hrule-inner"></span>
+			</div>
+		</div>
+	</div>
+
+	<div class="row text-center">
+		<div class="small-6 small-centered columns">
+			<h3 class="group-header">LINKS</h3>
+			<div class="hrule">
+				<span class="hrule-inner"></span>
+			</div>
+		</div>
+	</div>
+	<div class="row image-row">
 		<div class"small-6 small-centered columns">
 			<ul class="small-block-grid-3">
 				<li class="link-images">
 					<a href="http://github.com/thearrow">
-						<img src="img/webicon-github.svg"/>
+						<img class="link-image" src="img/webicon-github.svg"/>
 					</a>
 				</li>
 				<li class="link-images">
 					<a href="http://careers.stackoverflow.com/jakeklingler">
-						<img src="img/webicon-stackoverflow.svg"/>
+						<img class="link-image" src="img/webicon-stackoverflow.svg"/>
 					</a>
 				</li>
 				<li class="link-images">
 					<ul class="small-block-grid-2">
 						<li class="link-images-small">
 							<a href="https://www.quora.com/Jake-Klingler">
-								<img src="img/webicon-quora.svg"/>
+								<img class="link-image" src="img/webicon-quora.svg"/>
 							</a>
 						</li>
 						<li class="link-images-small">
 							<a href="https://www.facebook.com/jacob.klingler">
-								<img src="img/webicon-facebook.svg"/>
+								<img class="link-image" src="img/webicon-facebook.svg"/>
 							</a>
 						</li>
 						<li class="link-images-small">
 							<a href="http://www.goodreads.com/user/show/8126915-jake-klingler">
-								<img src="img/webicon-goodreads.svg"/>
+								<img class="link-image" src="img/webicon-goodreads.svg"/>
 							</a>
 						</li>
 						<li class="link-images-small">
 							<a href="http://www.flickr.com/photos/9829286@N07/">
-								<img src="img/webicon-flickr.svg"/>
+								<img class="link-image" src="img/webicon-flickr.svg"/>
 							</a>
 						</li>
 					</ul>		
