6290b58a7115e5c3d9f2c5f0c2ac269d64b385e4
Jake Klingler <thearrow3456@gmail.com>
2013-08-22 16:16:52 -0400

    refactor, DRY out scss, use grid for projects for easy addition, show gravatar in header

--- index.php
+++ index.php
@@ -35,186 +35,178 @@
 </head>
 <body>
 
-	<div class="top">
+	<div class="header-top">
 		<div class="row text-center header-container">
-			<h1 class="header">JAKE KLINGLER</h1>
-			<h3 class="small-header">I'm a programmer from Columbus, OH.</h3>
+			<h1>JAKE KLINGLER</h1>
+			<img src="http://www.gravatar.com/avatar/307a641b6c78cec6701f87d6c59534b9?s=100"/>
+			<h3>I'm a programmer from Columbus, OH.</h3>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="small-6 small-centered columns text-center">
 			<h3 class="group-header">PROJECTS</h3>
-			<div class="hrule">
-				<span class="hrule-inner"></span>
-			</div>
+			<div class="hrule"><span/></div>
 		</div>
+	</div>
 
-		<div class="small-12 small-centered columns">
-			<div class="row">
-				<div class="large-6 columns project-container">
+	<div class="row">
+		<div class="small-centered columns">
+			<ul class="large-block-grid-2 project-container">
+				<li>
 					<div class="row">
 						<div class="small-4 columns">
 							<a href="http://www.epic.com/software-phr.php">
-								<img class="project-image" src="img/projects/mychart.svg" alt="MyChart for Windows Phone"/>
+								<img src="img/projects/mychart.svg" alt="MyChart for Windows Phone" type='image/svg+xml' width="100%" height="100%"/>
 							</a>
 						</div>
 						<div class="small-8 columns">
 							<a href="http://www.epic.com/software-phr.php">
-								<h5 class="project-header">MYCHART FOR WINDOWS PHONE</h5>
+								<h5>MYCHART FOR WINDOWS PHONE</h5>
 							</a>
-							<p class="project-info">Epic – (May-Aug 2013)</p>
-							<ul class="desc-list">
-								<li class="description">MyChart allows patients to access their health information, view test results, send messages to their doctor, refill medications, and schedule appointments from their phone.</li>
-								<li>
-									<div class="tech-tag">C#</div>
-									<div class="tech-tag">.NET</div>
-									<div class="tech-tag">XAML</div>
+							<p>Epic – (May-Aug 2013)</p>
+							<ul>
+								<li>MyChart allows patients to access their health information, view test results, send messages to their doctor, refill medications, and schedule appointments from their phone.</li>
+								<li class="tech-tags">
+									<div>C#</div>
+									<div>.NET</div>
+									<div>XAML</div>
 								</li>
 							</ul>
 						</div>
 					</div>
-				</div>
-				<div class="large-6 columns project-container">
+				</li>
+				<li>
 					<div class="row">
 						<div class="small-4 columns">
 							<a href="https://github.com/thearrow/securitysystem-android">
-								<img class="project-image" src="img/projects/laser.svg" alt="Laser Tripwire System"/>
+								<img src="img/projects/laser.svg" alt="Laser Tripwire System" type='image/svg+xml' width="100%" height="100%"/>
 							</a>
 						</div>
 						<div class="small-8 columns">
 							<a href="https://github.com/thearrow/securitysystem-android">
-								<h5 class="project-header">LASER-TRIPWIRE SECURITY SYSTEM</h5>
+								<h5>LASER-TRIPWIRE SECURITY SYSTEM</h5>
 							</a>
-							<p class="project-info">The Ohio State University – (Feb-Apr 2013)</p>
-							<ul class="desc-list">
-								<li class="description">Built sensors using Raspberry Pi's and laser pointers that sent a push notification to a mobile app when tripped.</li>
-								<li class="description"></li>
-								<li>
-									<div class="tech-tag">RUBY ON RAILS</div>
-									<div class="tech-tag">JS/CSS/HTML</div>
-									<div class="tech-tag">PYTHON</div>
+							<p>The Ohio State University – (Feb-Apr 2013)</p>
+							<ul>
+								<li>Built sensors using Raspberry Pi's and laser pointers that sent a push notification to a mobile app when tripped.</li>
+								<li class="tech-tags">
+									<div>RUBY ON RAILS</div>
+									<div>JS/CSS/HTML</div>
+									<div>PYTHON</div>
 								</li>
 							</ul>
 						</div>
 					</div>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="large-6 columns project-container">
+				</li>
+				<li>
 					<div class="row">
 						<div class="small-4 columns">
-						<a href="https://github.com/thearrow/ai-financialANN">
-							<img class="project-image" src="img/projects/ann.svg" alt="Financial Market Neural Network"/>
-						</a>
+							<a href="https://github.com/thearrow/ai-financialANN">
+								<img src="img/projects/ann.svg" alt="Financial Market Neural Network" type='image/svg+xml' width="100%" height="100%"/>
+							</a>
 						</div>
 						<div class="small-8 columns">
 							<a href="https://github.com/thearrow/ai-financialANN">
-								<h5 class="project-header">FINANCIAL MARKET NEURAL NETWORK</h5>
+								<h5>FINANCIAL MARKET NEURAL NETWORK</h5>
 							</a>
-							<p class="project-info">The Ohio State University – (Feb-Apr 2013)</p>
-							<ul class="desc-list">
-								<li class="description">Designed and trained a recurrent neural network to predict the short-term movements of financial markets.</li>
-								<li class="description">(PyBrain, pandas, numpy, matplotlib)</li>
-								<li>
-									<div class="tech-tag">PYTHON</div>
+							<p>The Ohio State University – (Feb-Apr 2013)</p>
+							<ul>
+								<li>Designed and trained a recurrent neural network to predict the short-term movements of financial markets.</li>
+								<li>(PyBrain, pandas, numpy, matplotlib)</li>
+								<li class="tech-tags">
+									<div>PYTHON</div>
 								</li>
 							</ul>
 						</div>
 					</div>
-				</div>
-				<div class="large-6 columns project-container">
+				</li>
+				<li>
 					<div class="row">
 						<div class="small-4 columns">
-							<img class="project-image" src="img/projects/map.svg" alt="Various Web Projects"/>
+							<img src="img/projects/map.svg" alt="Various Web Projects" type='image/svg+xml' width="100%" height="100%"/>
 						</div>
 						<div class="small-8 columns">
-							<h5 class="project-header">VARIOUS WEB PROJECTS</h5>
-							<p class="project-info">The Ohio State University Libraries<br/> (Sept 2009 - Feb 2013)</p>
-							<ul class="desc-list">
-								<li class="description">Assisted with development of several applications for the OSU Libraries.</li>
-								<li class="description">(<a class="project-link" href="http://library.osu.edu/about/map/">Library map</a> in JS, a Rails scheduling app, <a class="project-link" href="http://library.osu.edu/buckeye-stroll/mobile">Buckeye Stroll</a> mobile app with Appcelerator)</li>
-								<li>
-									<div class="tech-tag">RUBY ON RAILS</div>
-									<div class="tech-tag">JS/CSS/HTML</div>
+							<h5>VARIOUS WEB PROJECTS</h5>
+							<p>The Ohio State University Libraries<br/> (Sept 2009 - Feb 2013)</p>
+							<ul>
+								<li>Assisted with development of several applications for the OSU Libraries.</li>
+								<li>(<a class="project-link" href="http://library.osu.edu/about/map/">Library map</a> in JS, a Rails scheduling app, <a class="project-link" href="http://library.osu.edu/buckeye-stroll/mobile">Buckeye Stroll</a> mobile app with Appcelerator)</li>
+								<li class="tech-tags">
+									<div>RUBY ON RAILS</div>
+									<div>JS/CSS/HTML</div>
 								</li>
 							</ul>
 						</div>
 					</div>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="large-6 columns project-container">
+				</li>
+				<li>
 					<div class="row">
 						<div class="small-4 columns">
 							<a href="img/hoban.png">
-								<img class="project-image" src="img/projects/hoban.svg" alt="Reunion Website"/>
+								<img src="img/projects/hoban.svg" alt="Reunion Website" type='image/svg+xml' width="100%" height="100%"/>
 							</a>
 						</div>
 						<div class="small-8 columns">
 							<a href="img/hoban.png">
-								<h5 class="project-header">HIGH SCHOOL REUNION WEBSITE</h5>
+								<h5>HIGH SCHOOL REUNION WEBSITE</h5>
 							</a>
-							<p class="project-info">Hoban Class of 1970 – (Jul-Aug 2010)</p>
-							<ul class="desc-list">
-								<li class="description">Designed and built a website for a high school class reunion using Drupal CMS.</li>
-								<li>
-									<div class="tech-tag">JS/CSS/HTML</div>
+							<p>Hoban Class of 1970 – (Jul-Aug 2010)</p>
+							<ul>
+								<li>Designed and built a website for a high school class reunion.</li>
+								<li class="tech-tags">
+									<div>JS/CSS/HTML</div>
 								</li>
 							</ul>
 						</div>
 					</div>
-				</div>
-				<div class="large-6 columns project-container">
+				</li>
+				<li>
 					<div class="row">
 						<div class="small-4 columns">
 							<a href="img/rateit.png">
-								<img class="project-image" src="img/projects/rateit.svg" alt="Rate-It iPhone App"/>
+								<img src="img/projects/rateit.svg" alt="Rate-It iPhone App" type='image/svg+xml' width="100%" height="100%"/>
 							</a>
 						</div>
 						<div class="small-8 columns">
 							<a href="img/rateit.png">
-								<h5 class="project-header">RATE-IT IPHONE APP</h5>
+								<h5>RATE-IT IPHONE APP</h5>
 							</a>
-							<p class="project-info">Carrick Enterprises – (Jun-Aug 2009)</p>
-							<ul class="desc-list">
-								<li class="description">Built a simple app for rating user-specified categories of items (e.g. wine, cat food, etc.)</li>
-								<li>
-									<div class="tech-tag">OBJECTIVE-C</div>
+							<p>Carrick Enterprises – (Jun-Aug 2009)</p>
+							<ul>
+								<li>Built a simple app for rating user-specified categories of items (e.g. wine, cat food, etc.)</li>
+								<li class="tech-tags">
+									<div>OBJECTIVE-C</div>
 								</li>
 							</ul>
 						</div>
 					</div>
-				</div>
-			</div>
-		</div>
+				</li>
+			</ul>
+		</div>	
 	</div>
 
 	<div class="row">
 		<div class="small-6 small-centered columns text-center">
 			<h3 class="group-header">RESUME</h3>
-			<div class="hrule">
-				<span class="hrule-inner"></span>
-			</div>
+			<div class="hrule"><span/></div>
 		</div>
-		<div class="small-12 large-8 small-centered columns">
-			<h5 class="resume-header">OBJECTIVE</h5>
-			<p class="resume-body">Seeking full-time employment after my graduation in December, 2013.</p>
+		<div class="small-12 large-8 small-centered columns resume-container">
+			<h5>OBJECTIVE</h5>
+			<p>Seeking a full-time software engineering position after my graduation in December, 2013.</p>
 
-			<h5 class="resume-header">EDUCATION</h5>
-			<p class="resume-body">
+			<h5>EDUCATION</h5>
+			<p>
 			The Ohio State University, Columbus, OH<br/>
 			B.S. Computer Science Engineering, Graduation: Dec, 2013<br/>
 			Overall GPA (4.00 scale): 3.5</p>
 
-			<h5 class="resume-header">WORK EXPERIENCE</h5>
-			<p class="resume-body">
+			<h5>WORK EXPERIENCE</h5>
+			<p>
 			Software Developer Intern (May, 2013 – August, 2013)<br/>
 			Epic Systems, Verona, WI<br/>
 			Designed and developed MyChart for Windows Phone 8 with two other interns.</p>
-			<p class="resume-body">
+			<p>
 			Student Programmer (September, 2009 – February, 2013)<br/>
 			The Ohio State University, Libraries Web Implementation Team, Columbus, OH<br/>
 			Assisted with programming tasks and development of projects for the Libraries Web Team</p>
@@ -224,46 +216,34 @@
 	<div class="row text-center">
 		<div class="small-6 small-centered columns">
 		<h3 class="group-header">CONTACT &amp; LINKS</h3>
-			<div class="hrule">
-				<span class="hrule-inner"></span>
-			</div>
+			<div class="hrule"><span/></div>
 		</div>
 	</div>
 	<div class="row image-row text-center">
 		<div class="small-centered columns">
 		<p><a href="mailto:thearrow3456@gmail.com" class="email-me">EMAIL ME</a></p>
-			<ul class="small-block-grid-3">
+			<ul class="small-block-grid-3 link-images">
 				<li>
 					<a href="http://github.com/thearrow">
-						<img class="link-image" src="img/webicon-github.svg" alt="My Github"/>
-					</a>
+						<img src="img/webicon-github.svg" alt="My Github"/></a>
 				</li>
 				<li>
 					<a href="http://careers.stackoverflow.com/jakeklingler">
-						<img class="link-image" src="img/webicon-stackoverflow.svg" alt="My StackOverflow"/>
-					</a>
+						<img src="img/webicon-stackoverflow.svg" alt="My StackOverflow"/></a>
 				</li>
-				<li class="link-images">
+				<li>
 					<ul class="small-block-grid-2">
-						<li>
-							<a href="https://www.quora.com/Jake-Klingler">
-								<img class="link-image-small" src="img/webicon-quora.svg" alt="My Quora"/>
-							</a>
+						<li><a href="https://www.quora.com/Jake-Klingler">
+								<img src="img/webicon-quora.svg" alt="My Quora"/></a>
 						</li>
-						<li>
-							<a href="http://www.linkedin.com/pub/jake-klingler/78/999/10b">
-								<img class="link-image-small" src="img/webicon-linkedin.svg" alt="My LinkedIn"/>
-							</a>
+						<li><a href="http://www.linkedin.com/pub/jake-klingler/78/999/10b">
+								<img src="img/webicon-linkedin.svg" alt="My LinkedIn"/></a>
 						</li>
-						<li>
-							<a href="http://www.goodreads.com/user/show/8126915-jake-klingler">
-								<img class="link-image-small" src="img/webicon-goodreads.svg" alt="My GoodReads"/>
-							</a>
+						<li><a href="http://www.goodreads.com/user/show/8126915-jake-klingler">
+								<img src="img/webicon-goodreads.svg" alt="My GoodReads"/></a>
 						</li>
-						<li>
-							<a href="http://www.flickr.com/photos/9829286@N07/">
-								<img class="link-image-small" src="img/webicon-flickr.svg" alt="My Flickr"/>
-							</a>
+						<li><a href="http://www.flickr.com/photos/9829286@N07/">
+								<img src="img/webicon-flickr.svg" alt="My Flickr"/></a>
 						</li>
 					</ul>		
 				</li>
