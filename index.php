b037a358ac03c631d15bd108bb46ae640f2bfbf1
Jake Klingler <thearrow3456@gmail.com>
2013-08-23 14:49:11 -0400

    uniform square widths, expand links to two even rows

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 	<div class="header-top">
 		<div class="row text-center header-container">
 			<h1>JAKE KLINGLER</h1>
-			<img src="http://www.gravatar.com/avatar/307a641b6c78cec6701f87d6c59534b9?s=100"/>
+			<img src="http://www.gravatar.com/avatar/307a641b6c78cec6701f87d6c59534b9?s=200"/>
 			<h3>I'm a programmer from Columbus, OH.</h3>
 		</div>
 	</div>
@@ -219,34 +219,28 @@
 			<div class="hrule"><span/></div>
 		</div>
 	</div>
-	<div class="row image-row text-center">
-		<div class="small-centered columns">
+	<div class="row text-center">
+		<div class="small-10 large-6 small-centered columns">
 		<p><a href="mailto:thearrow3456@gmail.com" class="email-me">EMAIL ME</a></p>
 			<ul class="small-block-grid-3 link-images">
-				<li>
-					<a href="http://github.com/thearrow">
+				<li><a href="http://github.com/thearrow">
 						<img src="img/webicon-github.svg" alt="My Github"/></a>
 				</li>
-				<li>
-					<a href="http://careers.stackoverflow.com/jakeklingler">
+				<li><a href="http://careers.stackoverflow.com/jakeklingler">
 						<img src="img/webicon-stackoverflow.svg" alt="My StackOverflow"/></a>
 				</li>
-				<li>
-					<ul class="small-block-grid-2">
-						<li><a href="https://www.quora.com/Jake-Klingler">
-								<img src="img/webicon-quora.svg" alt="My Quora"/></a>
-						</li>
-						<li><a href="http://www.linkedin.com/pub/jake-klingler/78/999/10b">
-								<img src="img/webicon-linkedin.svg" alt="My LinkedIn"/></a>
-						</li>
-						<li><a href="http://www.goodreads.com/user/show/8126915-jake-klingler">
-								<img src="img/webicon-goodreads.svg" alt="My GoodReads"/></a>
-						</li>
-						<li><a href="http://www.flickr.com/photos/9829286@N07/">
-								<img src="img/webicon-flickr.svg" alt="My Flickr"/></a>
-						</li>
-					</ul>		
+				<li><a href="http://www.linkedin.com/pub/jake-klingler/78/999/10b">
+						<img src="img/webicon-linkedin.svg" alt="My LinkedIn"/></a>
 				</li>
+				<li><a href="https://www.quora.com/Jake-Klingler">
+						<img src="img/webicon-quora.svg" alt="My Quora"/></a>
+				</li>
+				<li><a href="http://www.goodreads.com/user/show/8126915-jake-klingler">
+						<img src="img/webicon-goodreads.svg" alt="My GoodReads"/></a>
+				</li>
+				<li><a href="http://www.flickr.com/photos/9829286@N07/">
+						<img src="img/webicon-flickr.svg" alt="My Flickr"/></a>
+				</li>	
 			</ul>
 		</div>
 	</div>
